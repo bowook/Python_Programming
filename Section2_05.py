8692af579c1c7ee0d684416a4e0dceb5dca20845
bowook <mbosx316@naver.com>
2023-08-10 18:06:44 +0900

    init

--- Section2_05.py
+++ Section2_05.py
@@ -5,31 +5,18 @@
 
 #첫 번째 줄에는 자연수 N,M 주어짐, N,M은 4,6,8,12,20중 하나
 
-N,M = map(int, input().split())
-sum_count = []
-count : int = 0
-index = []
-high_percent = []
-for i in range(1,N+1):
-    #만약 N이 4가 나왔다라고 했을 때, i가 1
+N, M = map(int, input().split())
+count = [0] * (N*M)
+#N+M을 해주는 이유는,
+#두개의 정다면체의 주사위를 굴려서 나올 수 있는 눈의 합의 경우의 수이다.
+#1, 2, 3, 4
+#1, 2, 3, 4, 5, 6
+# => 2, 3, 4, 5, 6, 7, 8, 9, 10
+for i in range(1, N+1):
     for j in range(1, M+1):
-        #그러면 j도 1부터 M까지 다 더해서 리스트에 추가
-        sum_count.append(i+j)
+        #인덱스가 합, value는 개수
+        count[i+j]+=1
 
-print(sum_count)
-for i in range(0,len(sum_count)):
-    count = sum_count.count(sum_count[i])
-    index.append(count)
+count_sort = list(filter(lambda x: count[x] == max(count), range(len(count))))
 
-for i in range(0, len(index)):
-    if(index[i] < max(index)):
-        continue
-    elif(index[i] == max(index)):
-        if(sum_count[i] in high_percent) :
-            continue
-        else :
-            high_percent.append(sum_count[i])
-
-high_percent.sort()
-
-print(high_percent)
+print(count_sort)
